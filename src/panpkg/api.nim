aaaba5522bc87abfef3ddb83b2a4d223139d5efa
Wesley Moore <wes@wezm.net>
2023-01-10 21:15:20 +1000

    Improve PNG check and use template to branch on premultiply

--- src/panpkg/api.nim
+++ src/panpkg/api.nim
@@ -1,6 +1,5 @@
 import std/macros
 import std/math
-import std/os as os
 import std/unicode as unicode
 
 import aglet/rect
@@ -151,18 +150,24 @@ proc loadImage*(path: string): Image =
 
   var
     width, height, channels: int
-    img = stbi.load(path, width, height, channels, 4)
+    file: File
+    magic: array[8, byte]
+
+  doAssert open(file, path)
+  discard file.readBytes(magic, 0, 8)
+  file.setFilePos(0)
+  var img = stbi.loadFromFile(file, width, height, channels, 4)
+  file.close()
 
   assert channels == 4,
     "stb_image must always return an image with 4 channels, " &
     "please report this on pan's GitHub"
 
   # convert from RGBA to ARGB because cairo <3
-  # A somewhat crude check to dermine if the image is a png. If so
-  # then we need to pre-multiply alpha as PNGs are not pre-multipled
-  # but Cairo requires it.
-  let (_, _, ext) = os.splitFile(path)
-  let premultiply = ext.toLower == ".png"
+  # Check for the PNG "magic" byte sequence to see if the image is a PNG.
+  # If it is we need to pre-multiply alpha as PNGs are not pre-multipled
+  # and Cairo requires it.
+  let premultiply = magic == [0x89'u8, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a]
   rgbaToArgb(img, width, height, channels, premultiply)
 
   result = Image(
@@ -177,32 +182,38 @@ proc loadImage*(path: string): Image =
   )
 
 proc rgbaToArgb(img: var seq[byte], width, height, channels: int, premultiply: bool) =
-  {.push checks: off.}
+  template conversionLoop(premultiply: static bool) {.dirty.} =
+    {.push checks: off.}
 
-  for y in 0..<height:
-    for x in 0..<width:
-      let
-        r = channels * (x + y * width)
-        g = r + 1
-        b = g + 1
-        a = b + 1
-      if premultiply:
+    for y in 0..<height:
+      for x in 0..<width:
         let
-          rr: uint32 = img[r]
-          gg: uint32 = img[g]
-          bb: uint32 = img[b]
-          aa: uint32 = img[a]
-        (img[r], img[g], img[b], img[a]) =
-          when cpuEndian == bigEndian:
-            (img[a], byte((rr * aa + 128) div 255), byte((gg * aa + 128) div 255), byte((bb * aa + 128) div 255))
-          else:
-            (byte((bb * aa + 128) div 255'u32), byte((gg * aa + 128) div 255), byte((rr * aa + 128) div 255), img[a])
-      else:
-        (img[r], img[g], img[b], img[a]) =
-          when cpuEndian == bigEndian: (img[a], img[r], img[g], img[b])
-          else: (img[b], img[g], img[r], img[a])
+          r = channels * (x + y * width)
+          g = r + 1
+          b = g + 1
+          a = b + 1
+        when premultiply:
+          let
+            rr: uint32 = img[r]
+            gg: uint32 = img[g]
+            bb: uint32 = img[b]
+            aa: uint32 = img[a]
+          (img[r], img[g], img[b], img[a]) =
+            when cpuEndian == bigEndian:
+              (img[a], byte((rr * aa + 128) div 255), byte((gg * aa + 128) div 255), byte((bb * aa + 128) div 255))
+            else:
+              (byte((bb * aa + 128) div 255'u32), byte((gg * aa + 128) div 255), byte((rr * aa + 128) div 255), img[a])
+        else:
+          (img[r], img[g], img[b], img[a]) =
+            when cpuEndian == bigEndian: (img[a], img[r], img[g], img[b])
+            else: (img[b], img[g], img[r], img[a])
 
-  {.pop.}
+    {.pop.}
+
+  if premultiply:
+    conversionLoop(premultiply = true)
+  else:
+    conversionLoop(premultiply = false)
 
 proc getCairo(image: Image): ptr Context =
   if image.cairo.isNil:
